e7e8333d86e4e4a9d87e19d86058205c78a375eb
Chad Williamson <chad@dahc.us>
2014-11-09 16:46:07 -0600

    naming/comment tweaks

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 /* LCD: data on PORTB, E on C6, RS on C7
  * EEPROM: data on PORTB, CS on F0, OE on F1, WE on E6
+ *         addr 0-7 on PORTD, addr 8-10 on F4-6
  */
 #define F_CPU		16000000
 #define LCD_DELAY_MS	5
@@ -13,10 +14,10 @@
 void lcd_init();
 void lcd_print(const char *str);
 void lcd_printf(const char *format, ...);
-void lcd_send(unsigned char rs, unsigned char byte);
+void lcd_send(unsigned char rs, unsigned char data);
 void mem_init();
 unsigned char mem_read(int addr);
-unsigned char mem_write(int addr, unsigned char data);
+void mem_write(int addr, unsigned char data);
 
 int main()
 {
@@ -25,7 +26,7 @@ int main()
 	mem_init();
 	lcd_init();
 
-	for (addr = 0x0000; addr < 0x07F0; addr++)
+	/*for (addr = 0x0000; addr < 0x07F0; addr++)
 		mem_write(addr, 0x00);
 	mem_write(0x7F0, 0xB0);
 	mem_write(0x7F1, 0x90);
@@ -36,9 +37,9 @@ int main()
 	mem_write(0x7F6, 0xE6);
 	mem_write(0x7F7, 0x01);
 	for (addr = 0x07F8; addr < 0x0800; addr++)
-		mem_write(addr, 0x00);
+		mem_write(addr, 0x00);*/
 
-	for (addr = 0x07F0; addr < 0x07F8; addr++) {
+	for (addr = 0x0000; addr < 0x0800; addr++) {
 		if (addr % 8 == 0) {
 			_delay_ms(1000);
 			lcd_send(0, 0x01);
@@ -76,7 +77,7 @@ unsigned char mem_read(int addr)
 	return data;
 }
 
-unsigned char mem_write(int addr, unsigned char data)
+void mem_write(int addr, unsigned char data)
 {
 	DDRB = 0xFF;
 	DDRD = 0xFF;
@@ -88,8 +89,6 @@ unsigned char mem_write(int addr, unsigned char data)
 	PORTE = 0x40;			   /* WE off */
 	_delay_ms(MEM_DELAY_MS);
 	PORTF = 0x03;
-	
-	return data;
 }
 
 void lcd_init()
@@ -117,11 +116,11 @@ void lcd_print(const char *str)
 		lcd_send(1, *str++);
 }
 
-void lcd_send(unsigned char rs, unsigned char byte)
+void lcd_send(unsigned char rs, unsigned char data)
 {
 	DDRB = 0xFF;
 	DDRC = 0xC0;
-	PORTB = byte;
+	PORTB = data;
 	PORTC = (rs << 7) | 0x40;
 	_delay_ms(LCD_DELAY_MS);
 	PORTC = (rs << 7);
